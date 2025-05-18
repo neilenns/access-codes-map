3af47df1cb98cbe536b16e63474c2c56f275fc6d
Neil Enns <info@neilenns.com>
2025-05-18 14:51:57 -0700

    Dynamically load the LocationsMap component (#21)
    * Dynamically load the LocationsMap component
    Fixes #18
    
    * Coderabbit

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,5 @@
-import { Loading } from "@/components/loading";
 import { getAllLocations } from "@/db/locations";
-import { Suspense } from "react";
-import LocationsMap from "./locations-map";
+import ClientSection from "./client-section";
 
 export default async function Home() {
   try {
@@ -10,9 +8,7 @@ export default async function Home() {
     return (
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-          <Suspense fallback={<Loading />}>
-            <LocationsMap locations={locations} />
-          </Suspense>
+          <ClientSection locations={locations} />
         </main>
       </div>
     );
