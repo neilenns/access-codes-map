987a49c6683e9342846d1c43645ffc459a2af4e3
Neil Enns <info@neilenns.com>
2025-05-18 23:46:11 +0000

    Getting there

--- apps/web/src/lib/auth0.ts
+++ apps/web/src/lib/auth0.ts
@@ -1,9 +1,41 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
+import { jwtDecode } from "jwt-decode";
+import { ENV } from "./environment";
 
-// Initialize the Auth0 client
-export const auth0 = new Auth0Client({
-  authorizationParameters: {
-    scope: "openid profile email",
-    audience: process.env.AUTH0_AUDIENCE,
-  },
-});
+let auth0Client: Auth0Client | undefined;
+
+interface DecodedAccessToken {
+  permissions?: string[];
+}
+
+export const getAuth0Client = () => {
+  auth0Client ??= new Auth0Client({
+    authorizationParameters: {
+      scope: "openid",
+      audience: ENV.AUTH0_AUDIENCE,
+    },
+    // Before saving the session, decode the access token to get the permissions
+    // and add them to the session object.
+    // eslint-disable-next-line @typescript-eslint/require-await
+    async beforeSessionSaved(session, idToken) {
+      if (!idToken || !session.tokenSet.accessToken) {
+        return session;
+      }
+
+      // The permissions are automatically added to the accessToken by auth0.
+      const decodedToken = jwtDecode<DecodedAccessToken>(
+        session.tokenSet.accessToken,
+      );
+
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          permissions: decodedToken.permissions ?? [],
+        },
+      };
+    },
+  });
+
+  return auth0Client;
+};
