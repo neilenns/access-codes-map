2eb1c784f8805f07beb14433e913f75277b35396
Neil Enns <info@neilenns.com>
2025-05-18 23:52:57 +0000

    More cleanup

--- apps/web/src/app/map/client-section.tsx
+++ apps/web/src/app/map/client-section.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { Loading } from "@/components/loading";
+import { Spinner } from "@/components/ui/spinner";
 import { LocationsWithUsers } from "@/db/locations";
+import { useCheckPermissions } from "@/hooks/use-check-permissions";
+import { Permissions } from "@/types/permissions";
 import dynamic from "next/dynamic";
+import UnauthorizedPage from "./unauthorized";
 
 export interface ClientSectionProperties {
   locations: LocationsWithUsers;
@@ -14,5 +18,28 @@ const LocationsMap = dynamic(() => import("./locations-map"), {
 });
 
 export default function ClientSection({ locations }: ClientSectionProperties) {
+  const { permissionsStatus, isLoading } = useCheckPermissions(
+    Permissions.ViewCodes,
+  );
+
+  if (isLoading) {
+    return (
+      <main
+        className="flex min-h-screen flex-col items-center justify-center text-center px-4 py-4"
+        aria-label="Logging in"
+      >
+        <div role="status">
+          <Spinner size="medium" aria-live="polite">
+            <span>Logging in...</span>
+          </Spinner>
+        </div>
+      </main>
+    );
+  }
+
+  if (!permissionsStatus[Permissions.ViewCodes]) {
+    return <UnauthorizedPage />;
+  }
+
   return <LocationsMap locations={locations} />;
 }
--- apps/web/src/app/map/layout.tsx
+++ apps/web/src/app/map/layout.tsx
@@ -1,8 +1,4 @@
-import { Spinner } from "@/components/ui/spinner";
-import { useCheckPermissions } from "@/hooks/use-check-permissions";
-import { Permissions } from "@/types/permissions";
 import type { Metadata } from "next";
-import UnauthorizedPage from "./unauthorized";
 
 export const metadata: Metadata = {
   title: "Access codes map",
@@ -14,28 +10,5 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { permissionsStatus, isLoading } = useCheckPermissions(
-    Permissions.ViewCodes,
-  );
-
-  if (isLoading) {
-    return (
-      <main
-        className="flex min-h-screen flex-col items-center justify-center text-center px-4 py-4"
-        aria-label="Logging in"
-      >
-        <div role="status">
-          <Spinner size="medium" aria-live="polite">
-            <span>Logging in...</span>
-          </Spinner>
-        </div>
-      </main>
-    );
-  }
-
-  if (!permissionsStatus[Permissions.ViewCodes]) {
-    return <UnauthorizedPage />;
-  }
-
   return <div>{children}</div>;
 }
