0919ad933f2f6512f42d6b2cd813b781ef6cabb3
Neil Enns <info@neilenns.com>
2025-05-19 11:24:44 +0000

    Fix map layout

--- apps/web/src/app/map/locations-map.tsx
+++ apps/web/src/app/map/locations-map.tsx
@@ -28,28 +28,30 @@ export default function LocationsMap({ locations }: MapProperties) {
   }, [map]);
 
   return (
-    <MapContainer
-      className="w-full h-full"
-      center={[47.654_961_858_209_56, -122.252_018_473_532_25]}
-      zoom={11}
-      scrollWheelZoom={true}
-      ref={storeMapReference}
-    >
-      <LayersControl position="topright">
-        <LayersControl.BaseLayer checked name="Roads">
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </LayersControl.BaseLayer>
-        <LayersControl.BaseLayer name="Satellite">
-          <TileLayer
-            attribution='Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
-            url="https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
-          />
-        </LayersControl.BaseLayer>
-      </LayersControl>
-      <LocationMarkers locations={locations} />
-    </MapContainer>
+    <div className="h-screen w-full">
+      <MapContainer
+        className="h-full w-full"
+        center={[47.654_961_858_209_56, -122.252_018_473_532_25]}
+        zoom={11}
+        scrollWheelZoom={true}
+        ref={storeMapReference}
+      >
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer checked name="Roads">
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Satellite">
+            <TileLayer
+              attribution='Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
+              url="https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
+            />
+          </LayersControl.BaseLayer>
+        </LayersControl>
+        <LocationMarkers locations={locations} />
+      </MapContainer>
+    </div>
   );
 }
