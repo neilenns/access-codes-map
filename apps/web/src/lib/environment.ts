1b76e4564534883db0a18ddbc67aaf0578b1c400
Neil Enns <info@neilenns.com>
2025-05-24 12:24:07 -0700

    Fix drizzle launch (#70)
    * Fix drizzle launch
    Fixes #28
    
    * Completely different approach
    
    * Default to production

--- apps/web/src/lib/environment.ts
+++ apps/web/src/lib/environment.ts
@@ -1,62 +1,65 @@
 import z from "zod";
 import { auth0url } from "./auth0-url";
 
-// Shared fields for both configurations
-const sharedFields = {
-  NODE_ENV: z
-    .enum(["development", "production", "test"])
-    .default("development"),
-  APP_BASE_URL: z.string().url(),
-  AUTH0_AUDIENCE: auth0url.optional(),
-  AUTH0_CLIENT_SECRET: z.string().optional(),
-  AUTH0_CLIENT_ID: z.string().optional(),
-  AUTH0_DOMAIN: auth0url.optional(),
-  AUTH0_SECRET: z.string().optional(), // To generate this use `openssl rand -hex 32`
-  CLOUDFLARE_ACCOUNT_ID: z.string().optional(),
-  CLOUDFLARE_DATABASE_ID: z.string().optional(),
-  CLOUDFLARE_D1_TOKEN: z.string().optional(),
-  DISABLE_AUTH: z
-    .preprocess((value) => value === "true" || value === "1", z.boolean())
-    .default(false),
-};
-
-// Local DB config
-const localDatabaseSchema = z.object({
-  LOCAL_DB_PATH: z.string(),
-  ...sharedFields,
-});
-
-// Cloudflare D1 config
-const cloudflareSchema = z.object({
-  LOCAL_DB_PATH: z.undefined().optional(),
-  ...sharedFields,
-});
-
 const environmentSchema = z
-  .union([localDatabaseSchema, cloudflareSchema])
+  .object({
+    LOCAL_DB_PATH: z.string().optional(),
+    APP_BASE_URL: z.string().url().optional(),
+    // The default here isn't strictly necessary, NextJS automatically sets NODE_ENV to development
+    // when running `next dev` and production when running all other `next` commands.
+    // See https://nextjs.org/docs/pages/guides/environment-variables#good-to-know.
+    NODE_ENV: z
+      .enum(["development", "production", "test"])
+      .default("production")
+      .optional(),
+    AUTH0_AUDIENCE: auth0url.optional(),
+    AUTH0_CLIENT_SECRET: z.string().optional(),
+    AUTH0_CLIENT_ID: z.string().optional(),
+    AUTH0_DOMAIN: auth0url.optional(),
+    AUTH0_SECRET: z.string().optional(), // To generate this use `openssl rand -hex 32`
+    CLOUDFLARE_ACCOUNT_ID: z.string().optional(),
+    CLOUDFLARE_DATABASE_ID: z.string().optional(),
+    CLOUDFLARE_D1_TOKEN: z.string().optional(),
+    DISABLE_AUTH: z
+      .preprocess((value) => value === "true" || value === "1", z.boolean())
+      .default(false)
+      .optional(),
+  })
   .superRefine((environment, context) => {
+    if (environment.LOCAL_DB_PATH) {
+      // If LOCAL_DB_PATH is set, nothing else matters
+      return;
+    }
+
     if (environment.DISABLE_AUTH && environment.NODE_ENV === "production") {
       context.addIssue({
         path: ["DISABLE_AUTH"],
         code: z.ZodIssueCode.custom,
-        message: "DISABLE_AUTH cannot be true in production environment",
+        message: "DISABLE_AUTH cannot be true in production environment.",
       });
     }
 
-    if (!environment.DISABLE_AUTH) {
-      for (const key of [
-        "AUTH0_DOMAIN",
-        "AUTH0_AUDIENCE",
-        "AUTH0_CLIENT_ID",
-        "AUTH0_CLIENT_SECRET",
-        "AUTH0_SECRET",
-      ] as const) {
-        // eslint-disable-next-line security/detect-object-injection
-        if (!environment[key]) {
+    // Check for required keys in production.
+    const requiredCloudflareKeys = [
+      "APP_BASE_URL",
+      "CLOUDFLARE_ACCOUNT_ID",
+      "CLOUDFLARE_DATABASE_ID",
+      "CLOUDFLARE_D1_TOKEN",
+      "AUTH0_DOMAIN",
+      "AUTH0_AUDIENCE",
+      "AUTH0_CLIENT_ID",
+      "AUTH0_CLIENT_SECRET",
+      "AUTH0_SECRET",
+    ];
+
+    if (environment.NODE_ENV === "production") {
+      for (const key of requiredCloudflareKeys) {
+        const value = environment[key as keyof typeof environment];
+        if (value === undefined || value === "") {
           context.addIssue({
             path: [key],
             code: z.ZodIssueCode.custom,
-            message: `${key} is required when DISABLE_AUTH is false`,
+            message: `${key} is required in production deployments.`,
           });
         }
       }
