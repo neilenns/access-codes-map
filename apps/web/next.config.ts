e0b52cebf06821edfa1f01c02081d21bfe6c45d4
Neil Enns <info@neilenns.com>
2025-05-26 20:12:57 +0000

    Try adding offline caching Fixes #81

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,13 +1,20 @@
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+import withSerwistInit from "@serwist/next";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {};
 
-export default nextConfig;
-
 // This enables local dev access to Cloudflare bindings. It comes from
 // https://opennext.js.org/cloudflare/get-started#11-develop-locally
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-
 void initOpenNextCloudflareForDev({
   environment: "dev",
 });
+
+const withSerwist = withSerwistInit({
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+  disable: process.env.NODE_ENV === "development",
+});
+
+// Apply Serwist to the Next config
+export default withSerwist(nextConfig);
