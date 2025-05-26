33d4202f66be40944e3ce12ffa3dfdaae34613a4
Neil Enns <info@neilenns.com>
2025-05-26 21:17:39 +0000

    Fix tailwindcss

--- apps/web/src/components/offline-bar.tsx
+++ apps/web/src/components/offline-bar.tsx
@@ -5,7 +5,7 @@ export default function OfflineBar() {
 
   return (
     !isOnline && (
-      <div className="fixed bottom-0 left-0 right-0 bg-red-500 text-white text-center p-2 z-1000">
+      <div className="fixed bottom-0 left-0 right-0 bg-red-500 text-white text-center p-2 z-[1000]">
         Offline
       </div>
     )
