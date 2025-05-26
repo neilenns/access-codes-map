138db1aef64b6759b2e4a9b94262fa4db14cbe1b
Neil Enns <info@neilenns.com>
2025-05-26 14:25:35 -0700

    Fix big strings warning from webpack (#85)
    Fixes #84

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,16 @@ import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import withSerwistInit from "@serwist/next";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  // From https://dopoto.github.io/blog/20250217-nextjs-serializing-big-strings
+  webpack: (config: { cache: { type: string } }) => {
+    config.cache = {
+      type: "memory",
+    };
+
+    return config;
+  },
+};
 
 // This enables local dev access to Cloudflare bindings. It comes from
 // https://opennext.js.org/cloudflare/get-started#11-develop-locally
